ee88d303a5c8746ae9ec45a3b3b6298ed2647193
Daniel Drew <daniel.drew2014@outlook.com>
2024-09-17 18:36:07 +0100

    first commit

---
+++ backend/src/module1.py
@@ -0,0 +1,5 @@
+
+
+
+def function1():
+    return "hello from function1"
---
+++ backend/tests/test_module1.py
@@ -0,0 +1,5 @@
+from src.module1 import function1
+
+
+def test_function1():
+    assert function1() == "hello from function1"
