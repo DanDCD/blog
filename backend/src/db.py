a879e93fb15d977393d073117ae7643e90eae3b2
Daniel Drew <daniel.drew2014@outlook.com>
2024-09-18 13:11:15 +0100

    Refactor User and Blog models and routes
    - Refactor User and Blog models to use type annotations for class attributes
    - Add blogs relationship to User model
    - Refactor user_routes and blog_routes to use consistent quotation marks
    - Update add_blog route to check if associated author exists
    - Update update_blog route to check if blog and new author exist
    - Update delete_blog route to return empty response with status code 204

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,38 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-
 
 db = SQLAlchemy()
 
 
-
 class User(db.Model):
     __tablename__ = "user"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    username: str = db.Column(db.String(50), nullable=False, unique=True)
+    password: str = db.Column(db.String(100), nullable=False)
+    blogs = db.relationship("Blog", backref="author", lazy=True)
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username
+            "id": self.id,
+            "username": self.username,
+            "blogs": [blog.to_dict() for blog in self.blogs],
         }
-
 
 
 class Blog(db.Model):
     __tablename__ = "blog"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    author_id: int = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    title: str = db.Column(db.String(100), nullable=False)
+    content: str = db.Column(db.Text, nullable=False)
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'title': self.title,
-            'content': self.content
+            "id": self.id,
+            "author_id": self.author_id,
+            "title": self.title,
+            "content": self.content,
         }
--- backend/src/routes.py
+++ backend/src/routes.py
@@ -3,51 +3,63 @@ from src.db import Blog
 from src.db import User
 from src.db import db
 
-user_routes = Blueprint('user_routes', __name__)
+user_routes = Blueprint("user_routes", __name__)
 
-@user_routes.route('/users', methods=['GET'])
+
+@user_routes.route("/users", methods=["GET"])
 def get_users():
     all_users = User.query.all()
     return jsonify([user.to_dict() for user in all_users])
 
 
-@user_routes.route('/users', methods=['POST'])
+@user_routes.route("/users", methods=["POST"])
 def add_user():
     data = request.json
-    new_user = User(username=data['username'], password=data['password'])
+    new_user = User(username=data["username"], password=data["password"])
     db.session.add(new_user)
     db.session.commit()
     return jsonify(new_user.to_dict()), 201
 
 
-blog_routes = Blueprint('blog_routes', __name__)
+blog_routes = Blueprint("blog_routes", __name__)
 
-@blog_routes.route('/blogs', methods=['GET'])
+
+@blog_routes.route("/blogs", methods=["GET"])
 def get_blogs():
     all_blogs = Blog.query.all()
     return jsonify([blog.to_dict() for blog in all_blogs])
 
-@blog_routes.route('/blogs', methods=['POST'])
+
+@blog_routes.route("/blogs", methods=["POST"])
 def add_blog():
     data = request.json
-    new_blog = Blog(title=data['title'], author_id=data['author_id'], content=data['content'])
+    # check the associated author exists
+    author = User.query.get_or_404(data["author_id"])
+    # add new blog
+    new_blog = Blog(title=data["title"], author_id=author.id, content=data["content"])
     db.session.add(new_blog)
     db.session.commit()
     return jsonify(new_blog.to_dict()), 201
 
-@blog_routes.route('/blogs/<int:blog_id>', methods=['PUT'])
+
+@blog_routes.route("/blogs/<int:blog_id>", methods=["PUT"])
 def update_blog(blog_id):
+    # check the blog to update exists
     blog = Blog.query.get_or_404(blog_id)
     data = request.json
-    blog.title = data['title']
-    blog.author_id = data['author_id']
-    blog.content = data['content']
+    # check the user id to be put into this resource exists
+    new_author = User.query.get_or_404(data["author_id"])
+    # update blog
+    blog.title = data["title"]
+    blog.author_id = new_author.id
+    blog.content = data["content"]
     db.session.commit()
     return jsonify(blog.to_dict())
 
-@blog_routes.route('/blogs/<int:blog_id>', methods=['DELETE'])
+
+@blog_routes.route("/blogs/<int:blog_id>", methods=["DELETE"])
 def delete_blog(blog_id):
     blog = Blog.query.get_or_404(blog_id)
     db.session.delete(blog)
     db.session.commit()
-    return '', 204
+    return "", 204
